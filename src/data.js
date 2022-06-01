f0fc4939254e0dc6c4b6282e8174941834ddbc9b
BekirLubic <64695858+BekirLubic@users.noreply.github.com>
2022-06-01 22:21:32 +0200

    data

---
+++ src/data.js
@@ -0,0 +1,46 @@
+const data = {
+  kampanje: [
+    {
+      naziv: 'Pilići',
+      slug: 'pilici',
+      kategorija: 'Samoprivredjivanje',
+      vrijednost: 600,
+      skupljeno: 280,
+      pocetakAkcije: '30/05/2022',
+      krajAkcije: '30/06/2022',
+      lokacije: 'Turbe',
+      opisAkcije: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+      ',
+      slika: "/dobroba/public/images/koke.jpg",
+    },
+    {
+        naziv: 'Ovce',
+        slug: 'ovce',
+        kategorija: 'Samoprivredjivanje',
+        vrijednost: 1200,
+        skupljeno: 490,
+        pocetakAkcije: '30/06/2022',
+        krajAkcije: '30/07/2022',
+        lokacije: 'Goles',
+        opisAkcije: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+  
+        ',
+        slika: "/dobroba/public/images/ovce.jpg",
+      },
+      {
+        naziv: 'Koze',
+        slug: 'koze',
+        kategorija: 'Samoprivredjivanje',
+        vrijednost: 850,
+        skupljeno: 790,
+        pocetakAkcije: '30/07/2022',
+        krajAkcije: '30/08/2022',
+        lokacije: 'Dub',
+        opisAkcije: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+  
+        ',
+        slika: "/dobroba/public/images/koze.jpg",
+      },
+  ],
+};
